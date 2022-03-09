c431e6b0211a517e90ec76fa3d13fb64c463d1d0
Hugo <hugomichelbl@gmail.com>
2022-03-09 13:17:39 -0600

    loginwithdb

--- piip_ipn_api/piip/command/user.py
+++ piip_ipn_api/piip/command/user.py
@@ -1,10 +1,15 @@
+from sqlalchemy import null
 from piip.services.database.setup import session
 from datetime import date
 from piip.models import User
 
 
 def insertUser(_firstname, _lastname, _email, _school_id, _password):
-    user = User(email=_email, password=_password, dob=date.today(), first_name=_firstname, last_name=_lastname, school_id=_school_id)
-    session.add(user)
+    # Check if user already exits
+    user = session.query(User).filter_by(email=_email).first()
+    if user:
+        return 0
+    new_user = User(email=_email, password=_password, dob=date.today(), first_name=_firstname, last_name=_lastname, school_id=_school_id)
+    session.add(new_user)
     session.commit()
-    return user
+    return 1
--- piip_ipn_api/piip/routes/token.py
+++ piip_ipn_api/piip/routes/token.py
@@ -1,18 +1,23 @@
 from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, unset_jwt_cookies, jwt_required, JWTManager
-from flask import request, Flask, jsonify
+from flask import request
 from flask_restful import Resource
-
+from piip.models import User,Administrator
+from piip.services.database.setup import session
 
 class Token(Resource):
     def post(self):
-        email = request.form.get("email", default='',type=str)
-        password = request.form.get("password", default='',type=str)
-        if email == "hugo" and password == "hugo":
-            access_token = create_access_token(identity=email)
-            response = {"access_token":access_token, "role": "mentor"}
-            return response
-        if email == "alvaro" and password == "alvaro":
-            access_token = create_access_token(identity=email)
+        _email = request.form.get("email", default='',type=str)
+        _password = request.form.get("password", default='',type=str)
+
+        user = session.query(User).filter_by(email=_email).first()
+        admin = session.query(Administrator).filter_by(email=_email).first()
+
+        if user and user.password == _password:
+            access_token = create_access_token(identity=_email)
             response = {"access_token":access_token, "role": "user"}
             return response
-        return {"msg": "Wrong email or password"}, 401
+        if admin and admin.password == _password:
+            access_token = create_access_token(identity=_email)
+            response = {"access_token":access_token, "role": "mentor"}
+            return response
+        return {"error": "Wrong email or password"}, 401
