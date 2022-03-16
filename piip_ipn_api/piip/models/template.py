ff077026d218c6c9fd47f8768dbce2e9d6a05653
Alvaro <ram.mendoza.a.98@outlook.com>
2022-03-15 22:23:51 -0600

    Add models for new template section tables

--- piip_ipn_api/piip/models/template.py
+++ piip_ipn_api/piip/models/template.py
@@ -19,16 +19,28 @@ class Template(PIIPModel):
     position = Column(Integer)
 
 
+class TemplateSection(PIIPModel):
+    __tablename__ = "TEMPLATE_SECTION"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    description = Column(Text)
+    position = Column(Integer)
+    template_id = Column(Integer, ForeignKey(f"{DATABASE}.TEMPLATE.id"))
+
+    template = relationship("Template", foreign_keys=[template_id])
+
+
 class TemplateActivity(PIIPModel):
     __tablename__ = "TEMPLATE_ACTIVITY"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
     description = Column(Text)
-    template_id = Column(Integer, ForeignKey(f"{DATABASE}.TEMPLATE.id"))
+    template_section_id = Column(Integer, ForeignKey(f"{DATABASE}.TEMPLATE_SECTION.id"))
     activity_type_id = Column(Integer, ForeignKey(f"{DATABASE}.DICT_ACTIVITY_TYPE.id"))
     position = Column(Integer)
     external_reference = Column(Integer)
 
-    template = relationship("Template", foreign_keys=[template_id])
+    template_section = relationship("TemplateSection", foreign_keys=[template_section_id])
     activity = relationship("DictActivityType", foreign_keys=[activity_type_id])
