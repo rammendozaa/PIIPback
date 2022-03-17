3090e926a66c1b8eae858be4e88d50ed5c88f3fb
Hugo <hugomichelbl@gmail.com>
2022-03-16 19:09:38 -0600

    asd

--- piip_ipn_api/piip/constants.py
+++ piip_ipn_api/piip/constants.py
@@ -1,4 +1,10 @@
+'''
 USERNAME = "root"
 PASSWORD = "root"
+HOST = "127.0.0.1"
+DATABASE = "PIIP_pruebas"
+'''
+USERNAME = "admin"
+PASSWORD = "*&WcgpYU4-.{mt.-"
 HOST = "127.0.0.1"
 DATABASE = "PIIP_pruebas"
