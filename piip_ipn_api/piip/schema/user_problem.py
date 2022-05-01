eec3c3254e7ab7996281b859df3d41c62fbca4f2
Alvaro <ram.mendoza.a.98@outlook.com>
2022-04-30 19:28:04 -0500

    User problem schema

--- piip_ipn_api/piip/schema/user_problem.py
+++ piip_ipn_api/piip/schema/user_problem.py
@@ -1,13 +1,13 @@
-from piip.models import Problem
+from piip.models.user import UserProblem
 from piip.schema.base_schema import BaseSchema
 from marshmallow import fields
 from marshmallow.utils import EXCLUDE
 
 class UserProblemSchema(BaseSchema):
-    __model__ = Problem
+    __model__ = UserProblem
 
     id = fields.Integer(data_key='id')
     user_id = fields.String(data_key='user_id')
     problem_id = fields.String(data_key="problem_id")
     status_id = fields.String(data_key="status_id")
-    status_id = fields.String(data_key="finished_id")
+    finished_date = fields.String(data_key="finished_date")
