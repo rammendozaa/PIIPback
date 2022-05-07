cb7f1f6858a1da8c378bd9da47866c9157e25534
Hugo <hugomichelbl@gmail.com>
2022-05-07 15:37:17 -0500

    softskill

---
+++ piip_ipn_api/piip/schema/user_programming_topic.py
@@ -0,0 +1,12 @@
+from piip.models.user import UserProgrammingTopic
+from piip.schema.base_schema import BaseSchema
+from marshmallow import fields
+from marshmallow.utils import EXCLUDE
+
+class UserProgrammingTopicSchema(BaseSchema):
+    __model__ = UserProgrammingTopic
+
+    id = fields.Integer(data_key='id')
+    user_id = fields.String(data_key='user_id')
+    programming_topic_id = fields.String(data_key="programming_topic_id")
+    status_id = fields.String(data_key="status_id")
