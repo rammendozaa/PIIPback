992b1462860fac0f2a6da32f43930c1cea36c761
Alvaro <ram.mendoza.a.98@outlook.com>
2022-05-10 19:42:42 -0500

    Activity fixes

--- piip_ipn_api/piip/schema/__init__.py
+++ piip_ipn_api/piip/schema/__init__.py
@@ -7,7 +7,7 @@ from piip.schema.problem import ProblemSchema
 from piip.schema.school import SchoolSchema
 from piip.schema.user import UserSchema
 from piip.schema.questionnaire import QuestionnaireSchema
-from piip.schema.interview import InterviewSchema
+from piip.schema.interviews import InterviewSchema
 from piip.schema.question import SoftSkillQuestionSchema
 from piip.schema.topic import (
     ProgrammingTopicSchema,
