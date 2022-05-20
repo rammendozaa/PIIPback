b47c4963cb92807662d2dd6d7708999aa5ee7615
Alvaro <ram.mendoza.a.98@outlook.com>
2022-05-19 21:58:20 -0500

    Remove header

--- piip_ipn_api/piip/setup.py
+++ piip_ipn_api/piip/setup.py
@@ -79,7 +79,6 @@ def create_application(name):
     class localFlask(Flask):
         def process_response(self, response):
             response.headers["Access-Control-Allow-Origin"] = "*"
-            response.headers["Access-Control-Allow-Credentials"] =  True
             response.headers["Access-Control-Allow-Methods"] =  "GET,HEAD,OPTIONS,POST,PUT, DELETE"
             response.headers["Access-Control-Allow-Headers"] =  "Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
             return (response)
